18d8b689bf60e173f16c3c941d39671becb2a825
awendo <hongsheng9311@163.com>
2017-06-25 13:14:31 +0800

    加入显示天气信息的功能

---
+++ app/src/main/java/com/awen/coolweather/gson/Basic.java
@@ -0,0 +1,19 @@
+package com.awen.coolweather.gson;
+
+import com.google.gson.annotations.SerializedName;
+
+/**
+ * Created by Administrator on 2017/6/19.
+ */
+
+public class Basic {
+    @SerializedName("city")
+    public String cityName;
+    @SerializedName("id")
+    public String weatherId;
+    public Update update;
+    public class Update{
+        @SerializedName("loc")
+        public String updateTime;
+    }
+}
